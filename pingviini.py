3c99ed0618c7f472345aef8720edbdec9d621520
jansode <63640610+jansode@users.noreply.github.com>
2024-08-20 07:03:25 +0300

    add command line argument

--- pingviini.py
+++ pingviini.py
@@ -1,7 +1,7 @@
 import requests
-
-ALKO_URL = "https://www.alko.fi"
-SUBDIRECTORIES = ["punaviinit","roseviinit","valkoviinit","viinijuomat"]
+import sys
+URL = "https://alko.fi"
+ALKO_SUBDIRECTORIES = ["punaviinit","roseviinit","valkoviinit","viinijuomat"]
 
 def webpage_exists(url):
     try:
@@ -17,19 +17,31 @@ def webpage_exists(url):
         return False
 
 def main():
-	print("Pinging "+ALKO_URL)
+
+	if len(sys.argv) < 2:
+		exit(1)
+		
+	if sys.argv[1] != "alko":
+		print("There is no wine at "+ URL)
+		exit(1)
+
+	print("Pinging "+ URL)
+	
+	
 	
 	nr_successes = 0
 	
-	for subdirectory in SUBDIRECTORIES:
-		if webpage_exists(ALKO_URL + "/" + subdirectory):
+	for subdirectory in ALKO_SUBDIRECTORIES:
+		if webpage_exists(URL + "/" + subdirectory):
 			print("Reply from alko/tuotteet/"+subdirectory)
 			nr_successes = nr_successes + 1
 		else:
 			print("Subdirectory "+subdirectory+" not found.")
 
 	if nr_successes == 0:
-		print("There is no wine.")
+		print("There is no wine in alko.")
+	else:
+		print("There is wine in alko.")
 	
 if __name__ == '__main__':
 	main()
